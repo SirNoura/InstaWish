aef0e9a4c40d674d0288b518842ab5046cb544c3
SirNoura <65782557+SirNoura@users.noreply.github.com>
2021-02-15 14:33:17 +0000

    profile done now work work work

--- app/src/main/java/slim/project/instawish/User.kt
+++ app/src/main/java/slim/project/instawish/User.kt
@@ -6,18 +6,27 @@ class User {
     var tipo : String?   = null
     var email : String? = null
     var urlImage : String? = null
+    var morada : String? = null
+    var codpos : String? = null
+    var andar : String? = null
 
     constructor(
             nome: String?,
             tipo: String?,
             email: String?,
-            urlImage: String?
+            urlImage: String?,
+            morada: String?,
+            codpos: String?,
+            andar: String?
 
     ) {
         this.nome      = nome
         this.tipo      = tipo
         this.email     = email
         this.urlImage  = urlImage
+        this.morada    = morada
+        this.codpos    = codpos
+        this.andar     = andar
     }
 
     fun toHashMap() : HashMap<String, Any?>{
@@ -26,6 +35,9 @@ class User {
         hasMap["tipo"] = tipo
         hasMap["email"] = email
         hasMap["urlImage"] = urlImage
+        hasMap["morada"] = morada
+        hasMap["codpos"] = codpos
+        hasMap["andar"] = andar
 
         return hasMap
     }
@@ -36,7 +48,10 @@ class User {
                     hashMap["nome"].toString(),
                     hashMap["tipo"].toString(),
                     hashMap["email"].toString(),
-                    hashMap["urlImage"].toString()
+                    hashMap["urlImage"].toString(),
+                    hashMap["morada"].toString(),
+                    hashMap["codpos"].toString(),
+                    hashMap["andar"].toString()
             )
             return item
         }
